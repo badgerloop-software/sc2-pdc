3c6a9acb6c4a6333966a85c361e45f962817f4e2
Aditya YV <adityayv802@gmail.com>
2025-01-28 19:45:51 -0600

    Digital pins logic added

--- include/digital.h
+++ include/digital.h
@@ -1,6 +1,21 @@
 #ifndef __DIGITAL_H__
 #define __DIGITAL_H__
 
+#include "STM32TimerInterrupt_Generic.h"
+
+//Define macros for the digital pins
+
+//Outputs
+#define MCU_DIR             PA_6
+#define MCU_ECO             PB_1
+#define MCU_MC_ON           PA_2
+
+//Inputs
+#define MCU_SPEED_SIG       PA_8
+#define PRK_BRK_TELEM       PB_4
+
+#define IO_UPDATE_PERIOD    1000000 // 1 second
+
 struct Digital_Data {
     bool direction : 1;         // output
     bool mc_speed_sig : 1;      // input
@@ -9,10 +24,17 @@ struct Digital_Data {
     bool park_brake : 1;        // input
 };
 
+extern volatile Digital_Data digital_data;
+
 // read all the digital inputs
 void readDigital();
 
 // initialize all the digital pins
 void initDigital();
+
+// Set the value of output pins
+void setDirection(bool dir);
+void setEcoMode(bool eco);
+void setMCOn(bool mc_on);
 
 #endif
---
+++ src/digital.cpp
@@ -0,0 +1,45 @@
+#include "digital.h"
+#include <Arduino.h>
+// #include "const.h"
+
+// Ticker to poll input readings at fixed rate
+STM32TimerInterrupt dataReader(TIM15);
+
+void initDigital(){
+    //outputs
+    pinMode(MCU_DIR, OUTPUT);
+    pinMode(MCU_ECO, OUTPUT);
+    pinMode(MCU_MC_ON, OUTPUT);
+
+    //inputs
+    pinMode(MCU_SPEED_SIG, INPUT);
+    pinMode(PRK_BRK_TELEM, INPUT);
+
+    // start the Timer to periodically read IO
+    if(dataReader.attachInterruptInterval(IO_UPDATE_PERIOD, readDigital)) { 
+        printf("starting dataUpdater timer\n");
+    } else {
+        printf("problem starting dataUpdater timer\n");
+    }
+}
+
+void readDigital(){
+    // read all the digital inputs
+    digital_data.mc_speed_sig = digitalRead(MCU_SPEED_SIG);
+    digital_data.park_brake = digitalRead(PRK_BRK_TELEM);
+}
+
+void setDirection(bool dir){
+    digitalWrite(MCU_DIR, dir);
+    digital_data.direction = dir;
+}
+
+void setEcoMode(bool eco){
+    digitalWrite(MCU_ECO, eco);
+    digital_data.eco_mode = eco;
+}
+
+void setMCOn(bool mc_on){
+    digitalWrite(MCU_MC_ON, mc_on);
+    digital_data.mc_on = mc_on;
+}
