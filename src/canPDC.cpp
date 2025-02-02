a47fc79308b2fb9310bbbc18b0bcc63f68b95e41
Khiem Vu <kmvu2@wisc.edu>
2025-02-02 12:58:00 -0600

    analog CAN complete, embedded-pio included in platformio.ini

--- src/canPDC.cpp
+++ src/canPDC.cpp
@@ -8,7 +8,14 @@ void CANPDC::readHandler(CAN_message_t msg) {
 
 void CANPDC::sendPDCData() {
     readAnalog();
-
+    this->sendMessage(0x200, (void*)acc_out, sizeof(float));
+    this->sendMessage(0x201, (void*)regen_brake, sizeof(float));
+    this->sendMessage(0x202, (void*)lv_12V_telem, sizeof(float));
+    this->sendMessage(0x203, (void*)lv_5V_telem, sizeof(float));
+    this->sendMessage(0x204, (void*)lv_5V_current, sizeof(float));
+    this->sendMessage(0x205, (void*)current_in_telem, sizeof(float));
+    this->sendMessage(0x206, (void*)brake_pressure_telem, sizeof(float));
+    
     readDigital();
     this->sendMessage(0x207, (void*)digital_data, sizeof(digital_data));
 }
