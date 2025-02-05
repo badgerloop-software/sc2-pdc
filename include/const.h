d38a8fd9cfb90beec3af6c833ae1b7b9ba91925d
Khiem Vu <kmvu2@wisc.edu>
2025-02-04 21:24:34 -0600

    added INA180_CURRENT_MULTIPLIER constant and defined volatile floats in analogPins.cpp

--- include/const.h
+++ include/const.h
@@ -3,4 +3,6 @@
 
 #define IO_UPDATE_PERIOD 100000 // us
 
+#define INA180_CURRENT_MULTIPLIER 10 // multiplies analog current telem to convert it into a readable value
+
 #endif
--- src/analogPins.cpp
+++ src/analogPins.cpp
@@ -1,5 +1,15 @@
 #include "analogPins.h"
 
+// analog values
+volatile float acc_out = 0;
+volatile float regen_brake = 0;
+
+volatile float lv_12V_telem = 0;
+volatile float lv_5V_telem = 0;
+volatile float lv_5V_current = 0;
+volatile float current_in_telem = 0;
+volatile float brake_pressure_telem = 0;
+
 // Ticker to poll input readings at a fixed rate
 STM32TimerInterrupt dataUpdater(TIM2);
 
@@ -17,8 +27,8 @@ void initAnalog() {
 void readAnalog() {
     lv_12V_telem = readADC(ADC_CHANNEL_6) * 3.3 *  35.1/5.1; // PA_1
     lv_5V_telem = readADC(ADC_CHANNEL_12) * 3.3 * 15.1/5.1; // PA_7
-    lv_5V_current = readADC(ADC_CHANNEL_15) * 10; // PB_0
-    current_in_telem = readADC(ADC_CHANNEL_8) * 10; // PA_3
+    lv_5V_current = readADC(ADC_CHANNEL_15) * INA180_CURRENT_MULTIPLIER; // PB_0
+    current_in_telem = readADC(ADC_CHANNEL_8) * INA180_CURRENT_MULTIPLIER; // PA_3
     brake_pressure_telem = readADC(ADC_CHANNEL_5); // PA_0
 }
 
