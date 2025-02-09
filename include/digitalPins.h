aba94ab676bff212d00249e2146cbd60954a5048
Aditya YV <adityayv802@gmail.com>
2025-02-09 13:18:21 -0600

    Function name changes and other PR requests

--- include/digitalPins.h
+++ include/digitalPins.h
@@ -4,8 +4,6 @@
 #include "ina281.h"
 #include "PID.h"
 
-//Define macros for the digital pins
-
 //Outputs
 #define MCU_DIR                PA_6
 #define MCU_ECO                PB_1
@@ -19,21 +17,21 @@ struct Digital_Data {
     bool direction : 1;         // output
     bool mc_speed_sig : 1;      // input
     bool eco_mode : 1;          // output
-    bool mc_on : 1;             // output
+    bool mcu_mc_on : 1;         // output
     bool park_brake : 1;        // input
 };
 
 extern volatile Digital_Data digital_data;
 
 // read all the digital inputs
-void readDigital();
+void read_digital();
 
 // initialize all the digital pins
-void initDigital();
+void init_digital();
 
 // Set the value of output pins
-void setDirection(bool dir);
-void setEcoMode(bool eco);
-void setMCOn(bool mc_on);
+void set_direction(bool dir);
+void set_eco_mode(bool eco);
+void set_mcu_mc_on(bool mc_on);
 
 #endif
--- src/digitalPins.cpp
+++ src/digitalPins.cpp
@@ -6,8 +6,9 @@
 
 // Ticker to poll input readings at fixed rate
 STM32TimerInterrupt digitalReader(TIM7);
+volatile Digital_Data digital_data;
 
-void initDigital(){
+void init_digital(){
     //Other pin initialization
     pinMode(MCU_DIR, OUTPUT);
     pinMode(MCU_ECO, OUTPUT);
@@ -17,25 +18,30 @@ void initDigital(){
     pinMode(PRK_BRK_TELEM, INPUT);
 
     // start the Timer to periodically read IO
-    if(digitalReader.attachInterruptInterval(IO_UPDATE_PERIOD, readDigital)) { 
+    if(digitalReader.attachInterruptInterval(IO_UPDATE_PERIOD, read_digital)) { 
         printf("starting digitalReader timer\n");
     } else {
         printf("problem starting digitalReader timer\n");
     }
 }
 
-void readDigital(){
+void read_digital(){
     // read all the digital inputs
     digital_data.mc_speed_sig = digitalRead(MCU_SPEED_SIG);
     digital_data.park_brake = digitalRead(PRK_BRK_TELEM);
 }
 
-void setDirection(bool dir){
+void set_direction(bool dir){
     digitalWrite(MCU_DIR, dir);
     digital_data.direction = dir;
 }
 
-void setEcoMode(bool eco){
+void set_eco_mode(bool eco){
     digitalWrite(MCU_ECO, eco);
     digital_data.eco_mode = eco;
+}
+
+void set_mcu_mc_on(bool mc_on){
+    digital_data.mcu_mc_on = mc_on;
+    digitalWrite(MCU_MC_ON, mc_on);
 }
