170e2c65cc68da2c3afa44fb7fc5a95ab5ff444c
Aditya YV <adityayv802@gmail.com>
2025-02-18 20:52:49 -0600

    Fixed code after testing on board

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@
 HardwareTimer *tim;
 uint32_t channel;
 
+uint32_t dutyCycle;
+int simRPM;
+char buf[1];
+
 void setup(){
     //Serial related
     Serial.begin(115200);
@@ -16,47 +20,43 @@ void setup(){
     tim = new HardwareTimer(Instance);
 
     channel = STM_PIN_CHANNEL(pinmap_function(PWM_PIN, PinMap_PWM));
+    // Fixed 10% duty cycle, the speed depends on frequency
+    dutyCycle = 10;
+
+    simRPM = 0;
+    Serial.write("Starting\n");
 }
 
 void loop()
 {
-    float rpm = 0;
 
-    // Fixed 10% duty cycle, the speed depends on frequency
-    uint32_t dutyCycle = 10;
+    delay(1000);
+    // printf("\e[1;1H\e[2J");
+    Serial.printf("Simulating RPM: %i    \n", simRPM);
+    Serial.printf("Press i to increment, d to decrement\n");
 
-    int simRPM = 0;
-    char buf[1];
-    printf("Starting\n");
-
-    while(true)
-    {
-        delay(20);
-        printf("\e[1;1H\e[2J");
-        printf("Simulating RPM: %i    ", simRPM);
-        printf("Press i to increment, d to decrement");
-
-        if (Serial.available() > 0) {
-            buf[0] = Serial.read();
-            if (buf[0] == 'i') {
-                simRPM += STEP_SIZE;
-            } else if (buf[0] == 'd') {
-                simRPM -= STEP_SIZE;
-            }
-
-            if (simRPM < 0) simRPM = 0;
+    if (Serial.available() > 0) {
+        buf[0] = Serial.read();
+        if (buf[0] == 'i') {
+            simRPM += STEP_SIZE;
+        } else if (buf[0] == 'd') {
+            simRPM -= STEP_SIZE;
         }
 
-        // Divide by 60 to get rps, multiply by 48 to get ticks per second
-        // Period in ms is 1000 / ticks per second
-        // Roughly equivalent to 1333.33333 / rpm
-
-        float periodMS;
-        if (simRPM > 0) {
-            periodMS = 1333.333333 / simRPM;
-            tim->setPWM(channel, PWM_PIN, 1000/periodMS, dutyCycle);
-        } else {
-            tim->setPWM(channel, PWM_PIN, 0, dutyCycle);
-        }
+        if (simRPM < 0) simRPM = 0;
     }
+
+    // Divide by 60 to get rps, multiply by 48 to get ticks per second
+    // Period in ms is 1000 / ticks per second
+    // Roughly equivalent to 1333.33333 / rpm
+
+    float periodMS;
+    if (simRPM > 0) {
+        periodMS = 1333.333333 / simRPM;
+        tim->setPWM(channel, PWM_PIN, 1000/periodMS, dutyCycle);
+    } else {
+        tim->setPWM(channel, PWM_PIN, 1, 0);
+    }
+    
+    Serial.printf("Current frequency: %f Hz\n", 1000/periodMS);
 }
